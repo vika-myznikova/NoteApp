39ec2e79cdf0b5682c7a1f6c6150421fb3f6d438
junaer <junaers1996@gmail.com>
2024-12-08 23:41:12 +0300

    fix: create db

--- db/create_tables.py
+++ db/create_tables.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 from datetime import datetime
 
 from db.connect import engine
