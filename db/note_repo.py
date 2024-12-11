0e4686da0e827361b15dd9e444a33ca8116dcda1
junaer <junaers1996@gmail.com>
2024-12-11 22:56:58 +0300

    add note update and delete

--- db/note_repo.py
+++ db/note_repo.py
@@ -15,6 +15,29 @@ def get_all_user_notes(user_id: int):
     ).all()
     return notes_list
 
+def delete_note(note_id: int):
+    note = session.query(Note).filter_by(
+        id=note_id,
+    ).one_or_none()
+    if note:
+        session.delete(note)
+        session.commit()
+        return True
+    return False
+
+def update_note(note_id: int , title: str, content: str):
+    note = session.query(Note).filter_by(
+        id=note_id
+    ).one_or_none()
+    if not note:
+        return None
+    note.title = title
+    note.content = content
+    session.commit()
+
 # Примеры
 # evgen_note = create_note("my note title", "content maker", 3)
-# notes = get_all_user_notes(1)
+# notes = get_all_user_notes(3)
+# update_note(1, title="111", content="222")
+# delete_note(2)
+# print(notes)
